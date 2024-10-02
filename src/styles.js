1284b43ebfb330f655f05c2119806c08243ed7a1
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-10-02 19:03:23 -0300

    primeiro

---
+++ src/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #FAFAFA;
+    display: flex;
+    align-items: center;
+    justify-content:  center;
+`
+
+export const Content = styled.div`
+    background-color:  #FFFFFF;
+    width: 80%;
+    min-height: 350px;
+`
