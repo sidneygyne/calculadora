2e32ce6c076388af447c0d2c6ce9519b35400cb9
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-10-03 18:45:59 -0300

    estlizaçoes

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,25 +3,21 @@ import styled from "styled-components";
 export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
-    background-color: #AAAAFF;
     display: flex;
     align-items: center;
     justify-content: flex-end;
-    font-size: 24px;
-    font-family: 'Roboto';
-    
-
 
     input {
         width: 100%;
         height: 75px;
-        background-color: #AAFFAA;
+        background-color: #000000;
         border: 0;
         display: flex;
         flex-direction: column;
         align-items: flex-end;
+        text-align: end;
         padding: 0 10px;
-        font-size: 24px;
+        font-size: 35px;
         font-family: 'Roboto';
         color: #FFFFFF;
     }
